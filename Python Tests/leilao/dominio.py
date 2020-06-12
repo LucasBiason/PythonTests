bf1f11a214461df2f5def8d7c0f255b224fff4d0
Lucas Biason <biasonlucky@hotmail.com>
2020-06-12 10:38:37 -0300

    Aula 03 - Segunda Parte

--- Python Tests/leilao/dominio.py
+++ Python Tests/leilao/dominio.py
@@ -30,13 +30,21 @@ class Leilao:
         return self.__lances[:]
     
     def novo_lance(self,lance):
+        if self.__lances:
+            if self.__lances[-1].usuario.nome == lance.usuario.nome:
+                raise ValueError('O mesmo usuário não pode propor dois lances seguidos.')
+            
+            if self.__lances[-1].valor > lance.valor:
+                ultimo = self.__lances[-1].valor
+                raise ValueError(f'O valor do lance deve ser maior que o último: {ultimo}')
+        
         self.__lances.append(lance)
         
         if self.maior_lance is None:
             self.maior_lance = lance.valor
         elif lance.valor > self.maior_lance:
             self.maior_lance = lance.valor
-       
+    
         if self.menor_lance is None:
             self.menor_lance = lance.valor
         elif lance.valor < self.menor_lance:
--- Python Tests/leilao/test_leilao.py
+++ Python Tests/leilao/test_leilao.py
@@ -25,15 +25,16 @@ class TestLeilao(TestCase):
         self.assertEqual(self.leilao.maior_lance, maior_valor)
 
     def test_case001(self):
-        self.criar_lance('Gui', 150.0)
         self.criar_lance('Yuri', 100.0)
+        self.criar_lance('Gui', 150.0)
         self.avaliar(100.0 , 150.0)
 
     def test_case002(self):
+        ''' Não deve deixar entrar lance se for o valor for menor que o ultimo '''
         self.criar_lance('Vinicius', 200.0)
-        self.criar_lance('Gui', 150.0)
-        self.criar_lance('Yuri', 100.0)
-        self.avaliar(100.0 , 200.0)
+        with self.assertRaises(ValueError):
+            self.criar_lance('Gui', 150.0)
+        self.assertEqual(1, len(self.leilao.lances))
         
     def test_case003(self):
         ''' > Deve permitir propor lance caso não tenha lances '''
@@ -41,13 +42,17 @@ class TestLeilao(TestCase):
         self.assertEqual(1, len(self.leilao.lances))
         
         ''' > Deve permitir propor lance caso o ultimo usuario seja deferente '''
-        self.criar_lance('Yuri', 100.0)
+        self.criar_lance('Yuri', 200.0)
         self.assertEqual(2, len(self.leilao.lances))
         
+    def test_case004(self):
         ''' > Não deve permitir propor lance caso o ultimo usuario seja o mesmo '''
-        self.criar_lance('Gui', 300.0)
-        self.assertEqual(2, len(self.leilao.lances))
-        
+        self.criar_lance('Yuri', 100.0)
+        with self.assertRaises(ValueError):
+            self.criar_lance('Yuri', 300.0)
+        self.assertEqual(1, len(self.leilao.lances))
+            
+            
 
 #if __name__ == '__main__':
 #    unittest.main()
