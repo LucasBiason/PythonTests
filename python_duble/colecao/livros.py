72d405222b9a30baf434e4a1528b3631ec47240b
Lucas Biason <biasonlucky@hotmail.com>
2020-06-17 16:10:49 -0300

    Dublê Stub

--- python_duble/colecao/livros.py
+++ python_duble/colecao/livros.py
@@ -1,8 +1,25 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import logging
 
 def consultar_livros(autor):
-    preparar_dados_para_requisicao(autor)
-    return ""
+    dados = preparar_dados_para_requisicao(autor)
+    url = obter_url("https://buscador", dados)
+    ret = executar_requisicao(url)
+    return ret
 
 def preparar_dados_para_requisicao(autor):
-    return ""
+    pass
     
+def obter_url(url, dados):
+    pass
+
+def executar_requisicao(url):
+    try:
+        with urlopen(url, timeout=10) as resposta:
+            resultado = resposta.read().decode("utf-8")
+    except HTTPError as e:
+        logging.exception("Ao acessar '%s': %s" % (url, e))
+    else:
+        return resultado
+        
--- python_duble/tests/test_livros.py
+++ python_duble/tests/test_livros.py
@@ -1,11 +1,17 @@
-from unittest.mock import patch
-from python_duble.colecao.livros import consultar_livros
+import pytest
+from urllib.error import HTTPError
+from unittest.mock import patch, mock_open, Mock
+from unittest import skip
 
+from python_duble.colecao.livros import consultar_livros, executar_requisicao
+
+@skip("Vale quando consultar_livros estiver completo")
 def  test_consultar_livros_001():
     ''' Retorna dados com formato string '''
     resultado = consultar_livros("Agatha Christie")
     assert type(resultado) == str
     
+@skip("")
 def  test_consultar_livros_002():
     ''' Chama preparar dados para requisição uma vez e com os mesmos
     parâmetrosde consultar livros '''
@@ -13,3 +19,43 @@ def  test_consultar_livros_002():
     with patch('python_duble.colecao.livros.preparar_dados_para_requisicao') as duble:
         consultar_livros("Agatha Christie")
         duble.assert_called_once_with("Agatha Christie")
+
+@skip("")
+def test_consultar_livros_003():
+    ''' Chama obter url usando como parametro o retorno de preparar 
+    dados para requisicao'''
+    with patch("python_duble.colecao.livros.preparar_dados_para_requisicao") as duble_preparar:
+        dados = {"author": "Agatha Christie"}
+        duble_preparar.return_value = dados
+        with patch("python_duble.colecao.livros.obter_url") as duble_obter_url:
+            consultar_livros("Agatha Christie")
+            duble_obter_url.assert_called_once_with("https://buscador", dados)
+
+@skip("")
+def test_consultar_livros_004():
+    ''' Chama executar reuisição usando retorno do obter_url'''
+    with patch("python_duble.colecao.livros.obter_url") as duble_obter_url:
+        duble_obter_url.return_value = "https://buscador"
+        with patch("python_duble.colecao.livros.executar_requisicao") as duble_executar_requisicao:
+            consultar_livros("Agatha Christie")
+            duble_executar_requisicao.assert_called_once_with("https://buscador")
+
+class StubHTTPResponse:
+    def read(self):
+        return b""
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, param1, param2, param3):
+        pass
+    
+def stub_de_urlopen(url, timeout):
+    return StubHTTPResponse()
+
+def  test_consultar_livros_005():
+    ''' Executar Requisição retorna tipo string '''
+    with patch("python_duble.colecao.livros.urlopen", stub_de_urlopen):
+        print(stub_de_urlopen)
+        resultado = executar_requisicao("https://buscarlivros?autor=Jk+Rowlings")
+        assert type(resultado) == str
